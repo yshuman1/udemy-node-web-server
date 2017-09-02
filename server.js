fdc7c8f5d3c09b2cbc2bae5e3e18f7119aee10ad
yshuman1 <yshuman@loyalloops.com>
2017-09-02 01:56:35 -0700

    added projects page

--- server.js
+++ server.js
@@ -44,6 +44,12 @@ app.get('/about', (req, res) => {
   });
 });
 
+app.get('/projects', (req, res) => {
+  res.render('projects.hbs', {
+    pageTitle: 'Projects Page'
+  });
+});
+
 // /bad - send back json with errorMessage
 app.get('/bad', (req, res) => {
   res.send({
